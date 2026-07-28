2f2aa3e10f52920c52801953c4726e5a129e1d18
Leyzu <rizkifadli1908@students.amikom.ac.id>
2026-07-28 18:56:33 +0700

    database done, sytax done

--- catat_uang_sendiri.sql
+++ catat_uang_sendiri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2026 at 11:14 AM
+-- Generation Time: Jul 28, 2026 at 01:55 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
